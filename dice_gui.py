4c11584c65f8b78bf6a87575a26ea1dfffa9efc7
jonhunn <69512197+jonhunn@users.noreply.github.com>
2022-04-21 20:16:43 -0400

    Add files via upload

--- dice_gui.py
+++ dice_gui.py
@@ -8,9 +8,11 @@ from tkinter import OptionMenu
 # number of dice to roll
 options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+###### functions for random rolls ######
+
 
 def coinFlip():
-    total = r.randint(1,2)
+    total = r.randint(1, 2)
     if total % 2 == 0:
         messagebox.showinfo("Message", "Coin Flip: \n Heads")
     else:
@@ -22,56 +24,59 @@ def rollD4():
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,4)
+        bone = r.randint(1, 4)
         total += bone
         bones.append(bone)
 
     total = ("D4:\n" + str(bones) + "\n total:" + str(total))
     messagebox.showinfo("Message", total)
 
+
 def rollD6():
     total = 0
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,6)
+        bone = r.randint(1, 6)
         total += bone
         bones.append(bone)
 
     total = ("D6:\n" + str(bones) + "\n total:" + str(total))
     messagebox.showinfo("Message", total)
 
+
 def rollD8():
     total = 0
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,8)
+        bone = r.randint(1, 8)
         total += bone
         bones.append(bone)
 
     total = ("D8:\n" + str(bones) + "\n total:" + str(total))
     messagebox.showinfo("Message", total)
-    
+
 
 def rollD10():
     total = 0
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,10)
+        bone = r.randint(1, 10)
         total += bone
         bones.append(bone)
 
     total = ("D10:\n" + str(bones) + "\n total:" + str(total))
     messagebox.showinfo("Message", total)
 
+
 def rollD12():
     total = 0
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,12)
+        bone = r.randint(1, 12)
         total += bone
         bones.append(bone)
 
@@ -84,7 +89,7 @@ def rollD20():
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,20)
+        bone = r.randint(1, 20)
         total += bone
         bones.append(bone)
 
@@ -97,54 +102,52 @@ def rollD100():
     bones = []
 
     for i in range(howMany.get()):
-        bone = r.randint(1,100)
+        bone = r.randint(1, 100)
         total += bone
         bones.append(bone)
 
     total = ("D100:\n" + str(bones) + "\n total:" + str(total))
     messagebox.showinfo("Message", total)
 
-        
 
-
-#creates window
+# creates window
 window = tk.Tk()
 window.title("Dungeon Dice Roll!")
 window.geometry("210x250")
 
-##setting up the dropdown menu ("How many dice?")
+# setting up the dropdown menu ("How many dice?")
 howMany = IntVar()
 howMany.set(options[0])
-
 
 dropDown = OptionMenu(window, howMany, *options)
 dropDown.pack()
 
-coinButton = tk.Button(window, text = "Coin Flip", command = coinFlip)
+# buttons on the window
+coinButton = tk.Button(window, text="Coin Flip", command=coinFlip)
 coinButton.pack()
 
-d4Button = tk.Button(window, text = "D4", command = rollD4)
+d4Button = tk.Button(window, text="D4", command=rollD4)
 d4Button.pack()
 
-d6Button = tk.Button(window, text = "D6", command = rollD6)
+d6Button = tk.Button(window, text="D6", command=rollD6)
 d6Button.pack()
 
-d8Button = tk.Button(window, text = "D8", command = rollD8)
+d8Button = tk.Button(window, text="D8", command=rollD8)
 d8Button.pack()
 
 
-d10Button = tk.Button(window, text = "D10", command = rollD10)
+d10Button = tk.Button(window, text="D10", command=rollD10)
 d10Button.pack()
 
 
-d12Button = tk.Button(window, text = "D12", command = rollD12)
+d12Button = tk.Button(window, text="D12", command=rollD12)
 d12Button.pack()
 
 
-d20Button = tk.Button(window, text = "D20", command = rollD20)
+d20Button = tk.Button(window, text="D20", command=rollD20)
 d20Button.pack()
 
-d100Button = tk.Button(window, text = "D100", command = rollD100)
+d100Button = tk.Button(window, text="D100", command=rollD100)
 d100Button.pack()
 
 window.mainloop()
